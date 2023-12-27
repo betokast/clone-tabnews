09594025c314ebb521dc63ee8d86a6637b680114
ROBERTO CASTRO <37387009+betokast@users.noreply.github.com>
2023-12-27 18:03:58 +0000

    Identificação e captura do número máximo de conexões ao banco suportadas e seu respectivo teste

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,10 +3,10 @@ test("GET to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
 
   const updateAtParsed = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(updateAtParsed);
 
   expect(responseBody.depedencies.database.version).toEqual("16.1");
+  expect(responseBody.depedencies.database.max_connections).toEqual(100);
 });
