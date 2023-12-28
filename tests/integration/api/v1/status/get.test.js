97cce0832aae331a31c7dd68e89cce7e736efc81
ROBERTO CASTRO <37387009+betokast@users.noreply.github.com>
2023-12-28 02:16:00 +0000

    Ajustes para verificação de retorno do body da resposta

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,6 +3,7 @@ test("GET to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  // console.log(responseBody) --> podemos verificar o que está sendo retornado no body da resposta
 
   const updateAtParsed = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(updateAtParsed);
