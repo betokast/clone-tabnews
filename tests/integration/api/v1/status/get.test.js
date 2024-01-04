9c3963f4b56cee52c4e0a18df4b58b160e27ec44
ROBERTO CASTRO <37387009+betokast@users.noreply.github.com>
2024-01-04 02:19:09 +0000

    Finalização do endpoint `/api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,7 +3,7 @@ test("GET to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  //console.log(responseBody)
+  // console.log(responseBody)
 
   const updateAtParsed = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(updateAtParsed);
@@ -13,6 +13,3 @@ test("GET to api/v1/status should return 200", async () => {
   expect(responseBody.depedencies.database.opened_connections).toEqual(1);
 });
 
-test.only("Testando SQL Injection", async () => {
-  await fetch("http://localhost:3000/api/v1/status");  
-});
